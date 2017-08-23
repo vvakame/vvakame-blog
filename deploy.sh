77ee5c47e8de975c9105be52bd1537fdc99a8b73
vvakame <vvakame+dev@gmail.com>
2017-08-23 18:29:25 +0900

    Circle CIで利用するDocker imageを変更

--- deploy.sh
+++ deploy.sh
@@ -17,9 +17,6 @@ mkdir -p $GEN_DIR
 cp $TMPL_DIR/blog-hpa-rs.yml $TMPL_DIR/blog-service.yml $TMPL_DIR/ingress.yml $GEN_DIR
 cat $TMPL_DIR/blog-deployment.yml | sed "s#\${DOCKER_IMAGE}#${DOCKER_IMAGE}#" > $GEN_DIR/blog-deployment.yml
 
-gcloud --quiet components install kubectl
-gcloud config set project $PROJECT_ID
-gcloud config set compute/zone $ZONE
-gcloud --quiet container clusters get-credentials $CLUSTER_NAME
+gcloud --quiet --project $PROJECT_ID --zone $ZONE container clusters get-credentials $CLUSTER_NAME
 
 kubectl apply -f $GEN_DIR
